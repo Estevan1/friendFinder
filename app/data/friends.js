75347ff5dfb29f52cad21610378fbb00d5e24c57
estevan <estevan.renteria@gmail.com>
2019-07-31 01:06:37 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+
+var friendsArray = [
+    {
+        "name": "Don Johnson",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMjE0MDg5MDkzOV5BMl5BanBnXkFtZTcwNDYyNjY0MQ@@._V1_UY317_CR6,0,214,317_AL_.jpg",
+        "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    },
+    {
+        "name": "Samuel L Jackson",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": ["4","2","5","1","3","2","2","1","3","2"]
+    },
+    {
+        "name": "Rebecca De Mornay",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMTYzMjk0MzE1OV5BMl5BanBnXkFtZTYwODU3ODgz._V1_UY317_CR23,0,214,317_AL_.jpg",
+        "scores": ["5","2","2","2","4","1","3","2","5","5"]
+    },
+    {
+        "name": "Joe Rogan",
+        "photo": "https://pbs.twimg.com/profile_images/552307347851210752/vrXDcTFC_400x400.jpeg",
+        "scores": ["3","3","4","2","2","1","3","2", "2","3"]
+    },
+    {
+        "name": "Hannah Brown",
+        "photo": "https://s.yimg.com/ny/api/res/1.2/sXxJ9szPHD566iSQLzeR6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/cosmopolitan_438/ec2abd8dc07d1da9e84284199d6a8fb0",
+        "scores": ["4","3","4","1","5","2","5","3","1","4"]
+    },
+    {
+        "name": "Taylor Swift",
+        "photo": "https://s.abcnews.com/images/US/taylor-swift-gty-jef-190528_hpMain_1x1_384.jpg",
+        "scores": ["4","4","2","3","2","2","3","2","4","5"]
+    },
+    {
+        "name": "Brad Pitt",
+        "photo": "https://pmcvariety.files.wordpress.com/2019/07/shutterstock_editorial_10350209dp.jpeg?w=1000&h=563&crop=1",
+        "scores": ["1","1","1","1","1","1","1","1","1","1"]
+    },
+    {
+        "name": "Margot Robbie",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/9/98/Margot_Robbie_at_Somerset_House_in_2013_%28cropped%29.jpg",
+        "scores": ["1","2","5","3","4","5","1","3","2","3"]
+    }
+]
+
+module.exports = friendsArray;
